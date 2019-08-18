e41a1678ab76ed78b37c0cfe87a405707b662395
Jason Risch <jason@cardtapp.com>
2019-08-18 06:34:27 -0700

    Allow ifNull symbol chain

--- lib/aggregate.rb
+++ lib/aggregate.rb
@@ -11,6 +11,7 @@ Symbol.add_key(:and, "$and", "$and")
 Symbol.add_key(:nor, "$nor", "$nor")
 Symbol.add_key(:or, "$or", "$or")
 Symbol.add_key(:filter, "$filter", "$filter")
+Symbol.add_key(:if_null, "$ifNull", "$ifNull")
 
 # The base module for the gem under which all classes are namespaced.
 module Aggregate
