e68fe48b71f9acb6963206cdc1cde5370ad0affa
Jason Risch <jason@cardtapp.com>
2019-08-18 06:40:41 -0700

    Allow cond symbol chain

--- lib/aggregate.rb
+++ lib/aggregate.rb
@@ -12,6 +12,7 @@ Symbol.add_key(:nor, "$nor", "$nor")
 Symbol.add_key(:or, "$or", "$or")
 Symbol.add_key(:filter, "$filter", "$filter")
 Symbol.add_key(:if_null, "$ifNull", "$ifNull")
+Symbol.add_key(:cond, "$cond", "$cond")
 
 # The base module for the gem under which all classes are namespaced.
 module Aggregate
