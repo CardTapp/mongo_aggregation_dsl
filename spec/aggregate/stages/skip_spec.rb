d4899e8b6a22c4dacf28d0774fb958633a51f136
jaurand <jamie@cardtapp.com>
2023-01-31 09:35:45 -0800

    match mongo version

--- spec/aggregate/stages/skip_spec.rb
+++ spec/aggregate/stages/skip_spec.rb
@@ -2,20 +2,20 @@
 
 require "rails_helper"
 
-RSpec.describe Aggregate::Stages::Limit do
+RSpec.describe Aggregate::Stages::Skip do
   describe "#transpose" do
     it "should properly format" do
       expect(
-          Aggregate::Stages::Limit.new(5).transpose
-        ).to eq('$limit': 5)
+        Aggregate::Stages::Skip.new(5).transpose
+      ).to eq('$skip': 5)
     end
   end
   describe "schema" do
     it "should raise if option is not an integer" do
-      expect { Aggregate::Stages::Limit.new("test") }.to raise_error(ParamContractError)
+      expect { Aggregate::Stages::Skip.new("test") }.to raise_error(ParamContractError)
     end
     it "does not raise if option is an integer" do
-      expect { Aggregate::Stages::Limit.new(1) }.not_to raise_error
+      expect { Aggregate::Stages::Skip.new(1) }.not_to raise_error
     end
   end
 end
